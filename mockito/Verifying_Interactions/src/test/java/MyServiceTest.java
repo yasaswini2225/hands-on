c4f6dc8b50ac12456d0df9105fd4561c0fa96dc8
Angati-Yasaswini <yasaswiniangati22@gmail.com>
2026-06-30 23:29:59 +0530

    Verifying_Interactions

---
+++ mockito/Verifying_Interactions/src/test/java/MyServiceTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+public class MyServiceTest {
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
